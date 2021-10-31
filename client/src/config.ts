4d316d13bea5b3a0011a3225304d84afadb9dcc9
Tobenna Nwokike <tobennanwokike@gmail.com>
2021-10-31 19:52:25 +0100

    Completed Assignment

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '88tidp1wm4'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-3-u2lb-1.us.auth0.com',           // Auth0 domain
+  clientId: 'LF4TaT0iG5rUDGGq7DtVZv1zLEAwvXm6',  // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
